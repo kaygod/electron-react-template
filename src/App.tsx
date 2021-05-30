700dd89dce3f37ead9ba3536d9a68dfdadf21283
kaygod <184641801@qq.com>
2021-05-30 16:51:12 +0800

    update files

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import icon from '../assets/icon.svg';
 import './App.global.css';
+const path = require("path");
+const exec = require("child_process").exec;
 
 const Hello = () => {
+
+  const getSystem = ()=>{
+     
+    const script_path = path.join(__dirname, "scripts", "getSystem.sh"); 
+    
+    exec(`/bin/bash ${script_path}`, (error, stdout) => {
+       if(error){
+         console.log(error);
+       }else{
+         console.log(`stdout: ${stdout}`);
+       }
+    });
+
+  }  
+
   return (
     <div>
-       Hello world
+      <button onClick={getSystem}>Click me</button>
     </div>
   );
 };
