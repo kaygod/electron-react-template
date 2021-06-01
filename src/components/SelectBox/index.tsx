cb6776b2555fffadc50126b3bb46e0f5bb3b6e8d
13226625213 <570493561@qq.com>
2021-06-01 18:08:51 +0800

    公用组件

---
+++ src/components/SelectBox/index.tsx
@@ -0,0 +1,63 @@
+import React,{useState} from 'react';
+import style from './index.scss';
+
+type defaultProps = {
+    list: any[],
+    title:string,
+    titleAlwayShow:boolean,
+    ableInput:boolean,
+    placeHold?:string,
+    value:string,
+    inputType?:string,
+    onChange?: (v: any) => void; // 更新事件
+};
+
+const ctrlBtn = (props: defaultProps) => {
+    const [dropShow,setdropShow] = useState(false)
+  let {
+    list,
+    title,
+    ableInput=false,
+    titleAlwayShow=false,
+    value,
+    inputType='text',
+    placeHold='请选择',
+    onChange = () => {},
+  } = props;
+  return (
+    <div className={style.select}>
+        <div className={style.flexLeft+' '+ style.selectBar}>
+        <div className={style.containBox}>
+                {
+                    (titleAlwayShow||value)&&
+                    <div className={style.smTitle}>{title}</div>
+                }
+                {ableInput?
+                    <input className={style.in} type={inputType} value={value} placeholder={placeHold} onInput={(e)=>{onChange(e.currentTarget.value)}}></input>:
+                    <div className={style.textShow+' '+style.textEllips}>{value||placeHold}</div>
+                }
+        </div>
+        <div className={`${style.icon} ${style.flexCenter}`} onClick={()=>{setdropShow(!dropShow)}}>
+                1
+        </div>
+        </div>
+        {
+            dropShow&&<div className={style.selectBox}>
+            {
+                list.map((val)=>{
+                    return (
+                        <div className={style.selectItem+' '+style.textEllips} onClick={()=>{
+                            onChange(val.name) 
+                            setdropShow(false)
+                            }}>
+                            {val.name}
+                        </div>
+                    )
+                })
+            }
+        </div>}
+    </div>
+  );
+};
+
+export default ctrlBtn;
---
+++ src/components/UseRate/index.tsx
@@ -0,0 +1,101 @@
+import React,{useRef,useEffect} from 'react';
+import * as echarts from 'echarts';
+import 'echarts/lib/chart/line';
+import style from './index.scss';
+
+type defaultProps = {
+   procent:string|number,
+   title:string
+};
+const UseRate = (props: defaultProps) => {
+const gridRef = useRef<HTMLDivElement>(null);
+const {procent,title} = props
+useEffect(() => {
+    initChart()
+})
+const initChart = ()=>{
+    if (gridRef.current) {
+        const chart = echarts.init(gridRef.current);
+        const option:any = {
+            series: [{
+                type: 'gauge',
+                progress: {
+                    show: true,
+                    width: 20,
+                    itemStyle:{
+                        color:{
+                            colorStops:[
+                                {
+                                    offset: 0, color: '#087885' // 0% 处的颜色
+                                }, {
+                                    offset: 1, color: '#03c5ca' // 100% 处的颜色
+                                }]
+                        }
+                    }
+                },
+                axisLine: {
+                    show:true,
+                    lineStyle: {
+                        color:[[1, '#084356']],
+                        width: 20
+                    }
+                },
+                axisTick: {
+                    show: false
+                },
+                splitLine: {
+                    show:false,
+                },
+                axisLabel: {
+                    show:false
+                },
+                pointer:{
+                    show:false
+                },
+                anchor: {
+                    show: false,
+                },
+                title: {
+                    show: true,
+                    width:30,
+                    height:4,
+                    borderRadius:4,
+                    backgroundColor:'#01fff6',
+                    offsetCenter:['0','80%'],
+                     shadowColor:"#01fff6",
+                     shadowBlur:5
+                },
+                detail: {
+                    valueAnimation: true,
+                    fontSize: 30,
+                    color:'#03c5ca',
+                    textBorderColor :'#01fff6',
+                    textBorderWidth:1,
+                    textShadowBlur:5,
+                    offsetCenter: ['0', '10%'],
+                    formatter: function (value:any) {
+                        return value + '%'
+                    }
+                },
+                data: [{
+                    value: procent
+                }]
+            }]
+        }
+        chart.setOption(option)
+    }
+}
+  return ( 
+      <div className={style.warp}>
+        <div className={style.box}>
+          <div className={style.chartBox} ref={gridRef}></div>
+        </div>
+        <div className={style.title}>
+            {title}
+        </div>
+      </div>
+     
+  )
+};
+
+export default UseRate;
