81f3f06282b8d468f23d0dfa56e8120b1986f41b
kalaxy <5729485+kalaxy@user.noreply.gitee.com>
2021-06-02 14:40:12 +0800

    complete HdList static page

--- src/views/HdList/components/Chart/index.tsx
+++ src/views/HdList/components/Chart/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import UseRate from "components/UseRate/index";
+import styles from "./index.scss";
 
 const Chart = () => {
   return (
-    <div>
-        图表
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={`${styles.line} ${styles.first}`}>
+            <UseRate title="CPU使用率" procent={75}/>
+          </div>
+          <div className={styles.line}>
+            <UseRate title="内存使用率" procent={75}/>
+          </div>
+      </div>
     </div>
   );
 }
