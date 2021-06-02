036e3a8f7568701d9aac10a5aa8b85b1a92692e3
XIAOXUE WEI <1461124086@qq.com>
2021-06-02 11:03:21 +0800

    a

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from 'views/Home/index';
+import SwitchKey from 'views/SwitchKey/index';
 
 export default function RouteList() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        {/*<Route path="/" component={Home} />*/}
+        <Route path="/" component={SwitchKey} />
       </Switch>
     </Router>
   );
---
+++ src/views/SwitchKey/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./index.scss";
+
+const SwitchKey = () => {
+
+  return (
+    <div  className={styles.switchkey}>
+        xxxxx
+    </div>
+  );
+};
+
+export default SwitchKey;
