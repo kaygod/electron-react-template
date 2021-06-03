70d04b02672e3f1c34adc2bc6caf18863f8a166a
kalaxy <5729485+kalaxy@user.noreply.gitee.com>
2021-06-03 10:52:17 +0800

    config redux

--- src/store/reducers/Home/index.ts
+++ src/store/reducers/Home/index.ts
@@ -1,10 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetch } from 'util/common';
 
+enum stautsTyep {
+  initial = 1,
+  working = 2,
+  stop = 3,
+  completed = 4
+} 
+
+
+type defaultType = {
+  status:stautsTyep
+}
+
+
 export const counterSlice = createSlice({
   name: 'HdList',
   initialState: {
-    
+     status:1
   },
   reducers: {
     noOperate(state) {
