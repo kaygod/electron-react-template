bd092185e8d6acb5e44c62474d3b13094d690235
13226625213 <570493561@qq.com>
2021-06-07 13:52:55 +0800

    按钮操作事件

--- src/components/CutSwitch/index.tsx
+++ src/components/CutSwitch/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from "./index.scss";
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow, ipcRenderer } from 'electron'
 const CutSwitch = () => {
+  const close = ()=>{
+    ipcRenderer.send('close')
+  }
+  const min = ()=>{
+    ipcRenderer.send('min')
+  }
   return (
     <div className={styles.header}>
       <div className={styles.moveBar}></div>
       <div className={styles.switch}>
-                <div className={styles.small}><span className="iconfont icon-minus"></span></div>
-                <div className={styles.cut}><span className="iconfont icon-close"></span></div>
+                <div className={styles.small} onClick={min}><span className="iconfont icon-minus"></span></div>
+                <div className={styles.cut} onClick={close}><span className="iconfont icon-close"></span></div>
         </div>
     </div>
   );
--- src/main.dev.ts
+++ src/main.dev.ts
@@ -11,7 +11,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import path from 'path';
-import { app, BrowserWindow, shell,globalShortcut } from 'electron';
+import { app, BrowserWindow, shell,globalShortcut,ipcMain } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
@@ -73,6 +73,7 @@ const createWindow = async () => {
     height: 800,
     icon: getAssetPath('icon.png'),
     frame:false,
+    resizable:false,
     webPreferences: {
       nodeIntegration: true,
     },
@@ -137,3 +138,13 @@ app.on('activate', () => {
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow();
 });
+ipcMain.on('close',()=>{
+  if(mainWindow){
+    mainWindow.close()
+  }
+})
+ipcMain.on('min',()=>{
+  if(mainWindow){
+    mainWindow.minimize()
+  }
+})
