764761f57078eaae3249f9420a06129a0492a025
kalaxy <184641801@qq.com>
2021-06-08 16:21:16 +0800

    update global.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.global.css';
+import './App.global.scss';
 import store from './store/store';
 import { Provider } from 'react-redux';
 import RouteList from 'route/index';
