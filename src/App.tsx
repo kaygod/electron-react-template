705143940ab39f4abd0ea3747800389e2d8aefac
kalaxy <184641801@qq.com>
2021-06-08 14:57:41 +0800

    test package

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import store from './store/store';
 import { Provider } from 'react-redux';
 import RouteList from 'route/index';
 // @ts-ignore：无法被执行的代码的错误
-import Layout from "components/Layout/index.tsx";
+import Layout from "./components/Layout/index.tsx";
 
 
 export default function App() {
