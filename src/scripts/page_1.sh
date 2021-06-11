7f261a2eee942573b27eb65e6f37303f84059efd
kalaxy <184641801@qq.com>
2021-06-11 11:04:43 +0800

    update sudo

--- src/scripts/page_1.sh
+++ src/scripts/page_1.sh
@@ -3,3 +3,5 @@ p_ing=$(ps -ef |grep 'chia plots create' | grep -v "grep" | awk '$4==0'|wc -l )
 p_complete=$(find /data* -name plot*.plot |wc -l)
 #echo "$p_complete|$p_ing"
 echo "{'p_complete':'$p_complete','p_ing':'$p_ing'}"
+echo "{'p_complete':'$p_complete','p_ing':'$p_ing'}"
+echo "{'p_complete':'$p_complete','p_ing':'$p_ing'}"
