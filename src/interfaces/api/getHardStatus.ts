d27fa9ee2fb8a3d51b184bac6017cc64414291c8
13226625213 <570493561@qq.COM>
2021-06-15 15:01:20 +0800

    数据测试

--- src/interfaces/api/getHardStatus.ts
+++ src/interfaces/api/getHardStatus.ts
@@ -10,12 +10,11 @@ export const handler = async (params:any)=>{
     const result:any = await call(`list_3`, [page_no])//获取列表
     const result2:any = await call(`page_2`) //获取首页头部已批正在p的数量
     const evalRes = eval("("+result2+")")
-    console.log(result)
-    console.log(evalRes)
     data['list'] = formatExchange(result[0],['hard_disk','draw_num','draw_capacity'])//数据转换
     data['cpu_rate'] = evalRes.cpu_use
     data['memory_rate'] = evalRes.mem_use
     data['page_no'] = page_no
     data['total_page'] = Math.ceil(evalRes['dir_total']/1)
+    console.log(data)
     return data;
 }
