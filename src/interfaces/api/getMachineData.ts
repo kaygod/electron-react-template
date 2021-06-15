7829b1b2e7ca9a69606793fc4c2f8d4bc0ca556a
13226625213 <570493561@qq.COM>
2021-06-15 11:51:22 +0800

    数据测试

--- src/interfaces/api/getMachineData.ts
+++ src/interfaces/api/getMachineData.ts
@@ -7,7 +7,7 @@ export const handler = async (params:any)=>{
     const {page_no,type} = params
     //the two params must be array
     const data:any = {}
-    const result:any = await call(`$list_${page_no}`)//获取列表
+    const result:any = await call(`list_${page_no}`)//获取列表
     const result2:any = await call(`page_1`) //获取首页头部已批正在p的数量
     data['list'] = formatExchange(result[0],['k_value','file_name','status'])//数据转换
     data['working_tasks'] = eval("("+result2+")")['p_ing']
