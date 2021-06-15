880a44055de4d0e7e3288677505077d27c61cd19
XIAOXUE WEI <1461124086@qq.com>
2021-06-15 11:02:26 +0800

    x

--- src/views/SwitchKey/index.tsx
+++ src/views/SwitchKey/index.tsx
@@ -5,13 +5,13 @@ import CtrlBtn from "components/CtrlBtn/index"
 
 const SwitchKey = () => {
   return (
-    <div  className={styles.switchkey}>
+    <div  className={styles.switchKey}>
         <div className={styles.mid_box}>
-            <div className={styles.alltext}>
+            <div className={styles.allText}>
                <div className={styles.title}>请选择您的P盘公钥</div>
                <div className={styles.choice}><SelectBox titleAlwayShow={true} title="farm key" value="" ableInput={true} placeHold="请选择/输入farm key" list={[]}/></div>
                <div className={styles.choice}><SelectBox ableDrag={false} titleAlwayShow={true} title="pool key" value="" ableInput={true} placeHold="请选择/输入pool key" list={[]}/></div>
-               <div className={styles.btn}><CtrlBtn onlyBtn={true} labelStart="确认" status={true}/></div>
+               <div className={styles.btn}><CtrlBtn /></div>
             </div>
         </div>
     </div>
@@ -19,3 +19,4 @@ const SwitchKey = () => {
 };
 
 export default SwitchKey;
+                                             
