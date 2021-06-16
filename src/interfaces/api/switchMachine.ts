3b12fcc59eeff115e6329641d8314f36ed8fa516
13226625213 <570493561@qq.COM>
2021-06-16 14:02:56 +0800

    数据测试

---
+++ src/interfaces/api/switchMachine.ts
@@ -0,0 +1,12 @@
+import { call,formatExchange } from "util/common";
+
+/**
+ * 执行函数
+ */
+export const handler = async (params:any)=>{
+    const data:any = {}
+    // const result:any = await call(`list_1`)
+    // const list = formatExchange(result[0],['k_value','file_name','status'])
+    data['result'] = 1
+    return data;
+}
