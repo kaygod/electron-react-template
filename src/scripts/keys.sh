ae64f0e0cfb109177c995c051744c0585c77492f
13226625213 <570493561@qq.COM>
2021-06-17 14:33:56 +0800

    测试数据

--- src/scripts/keys.sh
+++ src/scripts/keys.sh
@@ -3,3 +3,4 @@ msg=$(/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon/chia keys show
 farmer_keys=${msg#*: }
 msg=$(/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon/chia keys show|tail -n +6 | head -n 1)
 pool_keys=${msg#*: }
+echo "{'farmer_keys':'$farmer_keys','pool_keys':'$pool_keys'}"
