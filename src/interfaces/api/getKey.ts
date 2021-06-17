90b1d160cec9793bed61fa8b95411b11107cbf74
13226625213 <570493561@qq.COM>
2021-06-17 14:26:38 +0800

    测试数据

---
+++ src/interfaces/api/getKey.ts
@@ -0,0 +1,13 @@
+import { call } from "util/common";
+
+/**
+ * 执行函数
+ */
+export const handler = async (params:any)=>{
+    //the two params must be array
+    const data:any = {}
+    const result:any = await call(`keys`)//获取密钥列表
+    const evalRes = eval("("+result+")")
+    console.log(evalRes)
+    return data;
+}
