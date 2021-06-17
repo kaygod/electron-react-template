887c7cba01fd48727bf5cdec75456bc5f800cbfa
13226625213 <570493561@qq.COM>
2021-06-17 17:21:20 +0800

    合并

--- src/interfaces/api/stop.ts
+++ src/interfaces/api/stop.ts
@@ -4,9 +4,12 @@ import { call,formatExchange } from "util/common";
  * 执行函数
  */
 export const handler = async (params:any)=>{
-    const id = params.id
-    const result:any = await call(`stop`)
+    // const data:any = {}
+    // const result:any = await call(`list_1`)
     // const list = formatExchange(result[0],['k_value','file_name','status'])
-    const evalRes = eval("("+result+")")
+    // const result:any = await call(`stop`)
+    // const evalRes = eval("("+result+")")
+    const evalRes:any = {}
+    evalRes['result'] = 1
     return evalRes;
 }
