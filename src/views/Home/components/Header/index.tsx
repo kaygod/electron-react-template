c2b9e535faa5b58ff244c1a8713bc36da7ef3c33
13226625213 <570493561@qq.COM>
2021-06-17 17:44:21 +0800

    合并

--- src/views/Home/components/Header/index.tsx
+++ src/views/Home/components/Header/index.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import styles from "./index.scss";
 import { useSelector } from 'react-redux'
 import { getter, updateType,getMachineDataAsync} from "store/reducers/Home";
+import { getter as globalGetter} from "store/reducers/Global";
 import { useDispatch } from 'react-redux';
 
 
 const Header = () => {
 
   const { end_tasks,working_tasks,type } = useSelector(getter);
+  const { chia_key } = useSelector(globalGetter);
   const dispatch = useDispatch()
   return (
     <div className={styles.header}>
         <div className={`${styles.headerItem} ${type=='1'&&styles.active}`} onClick={()=>{
           dispatch(updateType('1'))
-          dispatch(getMachineDataAsync())
+          chia_key && dispatch(getMachineDataAsync())
         }}>
             <div className={styles.itemName}>正在运行P盘数</div>
             <div className={styles.itemVal}>{working_tasks}</div>
         </div>
         <div className={`${styles.headerItem} ${type=='2'&&styles.active}`} onClick={()=>{
             dispatch(updateType('2'))
-            dispatch(getMachineDataAsync())
+            chia_key && dispatch(getMachineDataAsync())
           }}>
             <div className={styles.itemName}>已完成P盘数</div>
             <div className={styles.itemVal}>{end_tasks}</div>
