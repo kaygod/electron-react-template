330cfccde3b2a54adc9c84c58bfeeb7a4a969d2a
13226625213 <570493561@qq.COM>
2021-06-17 15:19:56 +0800

    合并

--- src/interfaces/api/switchKey.ts
+++ src/interfaces/api/switchKey.ts
@@ -4,6 +4,7 @@ import { call,formatExchange } from "util/common";
  * 执行函数
  */
 export const handler = async (params:any)=>{
+    console.log(params)
     const data:any = {}
     // const result:any = await call(`list_1`)
     // const list = formatExchange(result[0],['k_value','file_name','status'])
