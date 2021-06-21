de4dac9b8697edb26fd868bc7a55e68543adfc56
13226625213 <570493561@qq.COM>
2021-06-21 17:49:55 +0800

    测试

--- src/interfaces/api/startWork.ts
+++ src/interfaces/api/startWork.ts
@@ -15,7 +15,7 @@ export const handler = async (params:any)=>{
     const k_type = params.k_type
     try{
         const chia_key = JSON.parse(localStorage.getItem('chia_key')||'{"farmer_keys":"","pool_keys":""}')
-        const result:any = await call(`start [k${k_type},${chia_key.farmer_keys},${chia_key.pool_keys}]`)
+        const result:any = await call(`start`,[`k${k_type}`,chia_key.farmer_keys,chia_key.pool_keys])
         const result_obj = eval("("+result+")")
         if(result_obj){
             data['result'] = result_obj.result
