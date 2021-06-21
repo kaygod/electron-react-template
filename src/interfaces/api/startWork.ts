ca994a155fb21d11adbc2e89b4fa77ca53beb9e1
13226625213 <570493561@qq.COM>
2021-06-21 17:42:54 +0800

    测试

--- src/interfaces/api/startWork.ts
+++ src/interfaces/api/startWork.ts
@@ -1,15 +1,25 @@
 import { call,formatExchange } from "util/common";
 
 /**
- * 执行函数
+ * 执行函数 
+ * 
+ * 
+ * 200 P盘已满
+ * 102 参数缺失
+ * 101 参数错误
  */
+
 export const handler = async (params:any)=>{
-    const data:any = {}
     console.log(123)
-    console.log(params.k_type)
-    console.log(localStorage.getItem('chia_key'))
-    // const result:any = await call(`start ${}`)
-    // const list = formatExchange(result[0],['k_value','file_name','status'])
-    data['result'] = 1
+    const data:any = {}
+    const k_type = params.k_type
+    const chia_key = JSON.parse(localStorage.getItem('chia_key')||'{"farmer_keys":"","pool_keys":""}')
+    const result:any = await call(`start k${k_type} ${chia_key.armer_keys} ${chia_key.pool_keys}`)
+    const result_obj = eval("("+result+")")
+    if(result_obj){
+        data['result'] = result_obj.result
+    }else{
+        data['result'] = 1
+    }
     return data;
 }
