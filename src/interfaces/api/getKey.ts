ca2070e3430e6660a71600315693f153bf8e0bde
13226625213 <570493561@qq.COM>
2021-06-22 11:22:18 +0800

    测试

--- src/interfaces/api/getKey.ts
+++ src/interfaces/api/getKey.ts
@@ -3,23 +3,17 @@ import { call } from "util/common";
 /**
  * 执行函数
  */
-let num = 0
-const getData = async()=>{
-    const result:any = await call(`keys`)//获取密钥列表
-    const evalRes = eval("("+result+")")
-    if(evalRes.farmer_keys==''&&num==0){
-        num++
-        getData()
-    }
-    if(evalRes.farmer_keys!=''){
-        return evalRes
-    }
-}
 export const handler = async (params:any)=>{
     //the two params must be array
     const data:any = []
-    let evalRes:any = await getData()
-   data.push(evalRes)
-   return data
+    let result:any = await call(`keys`)//获取密钥列表
+    let evalRes = eval("("+result+")")
+    if(evalRes.farmer_keys==''){
+        result = await call(`keys`)
+        evalRes = eval("("+result+")")
+    }
+    if(evalRes.farmer_keys!=''){
+        data.push(evalRes)
+    }
+    return data;
 }
-
