5edb65797bf66b191559637dffbad6342723374c
13226625213 <570493561@qq.COM>
2021-06-22 10:45:56 +0800

    测试

--- src/interfaces/api/getKey.ts
+++ src/interfaces/api/getKey.ts
@@ -3,13 +3,20 @@ import { call } from "util/common";
 /**
  * 执行函数
  */
+ const getData = async()=>{
+    const result:any = await call(`keys`)//获取密钥列表
+    const evalRes = eval("("+result+")")
+    return evalRes
+}
 export const handler = async (params:any)=>{
     //the two params must be array
     const data:any = []
-    const result:any = await call(`keys`)//获取密钥列表
-    const evalRes = eval("("+result+")")
-    console.log(result)
-    data.push(evalRes)
-    console.log(data)
+    let evalRes:any = getData()
+    if(evalRes.farmer_keys==''){
+        evalRes = getData()
+    }else{
+        data.push(evalRes)
+    }   
     return data;
 }
+
